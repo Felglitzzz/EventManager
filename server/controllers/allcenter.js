c223bb425bcec44c85f3338370666d34de17b9ac
felglitz <amandeolaoluwa@gmail.com>
2017-11-24 01:09:07 +0100

    ft-get-a-single-center

--- server/controllers/allcenter.js
+++ server/controllers/allcenter.js
@@ -1,15 +1,15 @@
 import centers from '../models/center.json';
-
-
-export default class getAllCenters {
-
+/**
+ * 
+ */
+export default class GetAllCenters {
     /**
    * @static
    * @param {req} -returns request object
    * @param {res} -returns response object
    * @returns {json} -returns a json object of all centers
    */
-  static getAll(req, res) {
+  static getAllCenters(req, res) {
       return res.json ({
           Centers: centers,
           error: false
