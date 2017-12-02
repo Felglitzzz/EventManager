4d82c84832391bf3d59a5e1a9474ce4d72f64761
felglitz <amandeolaoluwa@gmail.com>
2017-12-02 18:30:16 +0100

    fix linting

--- server/seeders/20171202162024-user.js
+++ server/seeders/20171202162024-user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 
 module.exports = {
-  up: (queryInterface) => {
+  up(queryInterface) {
     return queryInterface.bulkInsert('users', [{
       surname: 'admin',
       firstname: 'admin',
@@ -14,7 +14,7 @@ module.exports = {
     }], { individualHooks: true });
   },
 
-  down: (queryInterface) => {
+  down(queryInterface) {
     return queryInterface.bulkDelete('users', null, {});
   }
 };
