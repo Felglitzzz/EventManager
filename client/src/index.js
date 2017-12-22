f2bd5371c0823106de74a7eba557cc8790badc9b
felglitz <amandeolaoluwa@gmail.com>
2017-12-22 20:31:03 +0100

    add webpack configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/home.jsx';
+import Home from './components/home';
 
 ReactDOM.render(<Home />, document.querySelector('#app'));
