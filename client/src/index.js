ada8c7708bf39e353d75b936465802388c02b5cc
felglitz <amandeolaoluwa@gmail.com>
2018-01-10 16:47:16 +0100

    load client and server side in parallel

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import 'bootstrap';
 
 import Routes from './routes';
-import '../../node_modules/font-awesome/css/font-awesome.min.css';
+// import '../../node_modules/font-awesome/css/font-awesome.min.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../public/css/style.css';
 import store from './store/configureStore';
